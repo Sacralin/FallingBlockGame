9f14208e25e781901bee77c080acd9594ebfb216
Paul Mould <PM902@student.aru.ac.uk>
2023-10-03 16:01:05 +0100

    Add project files.

---
+++ FallingBlockGame/GameObject.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FallingBlockGame
+{
+    internal class GameObject
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public string Sprite { get; private set; }
+
+        public GameObject(int x, int y, string sprite)
+        {
+            X = x;
+            Y = y;
+            Sprite = sprite;
+        }
+
+        public virtual void Move(int dx, int dy, int maxX, int maxY)
+        {
+            int newX = X + dx;
+            int newY = Y + dy;
+
+            if (newX >= 0 && newX < maxX)
+            {
+                X = newX;
+            }
+
+            if (newY >= 0 && newY < maxY)
+            {
+                Y = newY;
+            }
+            //else if (newY > maxX) newY = maxX;
+        }
+
+        public virtual void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(Sprite);
+        }
+
+
+        
+    }
+
+    class Player : GameObject
+    {
+        public int HP { get; private set; }
+        public Player(int x, int y, string sprite, int hp) : base(x, y, sprite)
+        {
+            HP = hp;
+        }
+
+        public void CheckCollisionWith(GameObject other)
+        {
+            if (X == other.X && Y == other.Y)
+            {
+
+                HP -= 1;
+            }
+        }
+
+        public override void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(Sprite);
+            Console.ResetColor();
+        }
+
+    }
+
+    class Block : GameObject
+    {
+        public int Speed { get; private set; }
+        private int frameCounter;
+
+        public Block(int x, int y, string sprite, int speed) : base(x, y, sprite)
+        {
+            Speed = speed;
+            frameCounter = 0;
+        }
+
+
+        public void Update(Block block, int maxX, int maxY)
+        {
+            frameCounter++;
+            if (frameCounter % Speed == 0) // % looks at the decimal number.
+            {
+                Move(0, 1, maxX, maxY);
+                frameCounter = 0;
+            }
+        }
+        public override void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(Sprite);
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ FallingBlockGame/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+
+namespace FallingBlockGame
+{
+    internal class Program
+    {
+        static Player player;
+        static List<Block> blocks;
+
+
+        static Random random = new Random();
+        static bool isRunning = true;
+        static DateTime lastUpdateTime = DateTime.Now;
+        static double deltaTime;
+        static double gameTimeElapsed;
+        static double blockTimeElapsed;
+        static double blockSpawnTimer = 5000;
+
+        static int windowWidth = 40;
+        static int windowHeight = 20;
+
+        static void Main(string[] args)
+        {
+            Console.BufferHeight = Console.WindowHeight * 2;
+            Console.CursorVisible = false;
+            player = new Player (windowWidth / 2, windowHeight -1, "@", 100);
+            blocks = new List<Block>();
+
+            while (isRunning)
+            {
+                DateTime currentTime = DateTime.Now;
+                deltaTime = (currentTime - lastUpdateTime).TotalMilliseconds;
+                lastUpdateTime = currentTime;
+                gameTimeElapsed += deltaTime;
+                blockTimeElapsed += deltaTime;
+                Update();
+                Draw();
+                Thread.Sleep(100);
+            }
+        }
+
+        private static void Update()
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                switch (keyInfo.Key)
+                {
+
+                    case ConsoleKey.Q:
+                        isRunning = false;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        player.Move(-1, 0, windowWidth, windowHeight);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        player.Move(1, 0, windowWidth, windowHeight);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            foreach (Block block in blocks)
+            {
+                block.Update(block, windowWidth, windowHeight);
+                player.CheckCollisionWith(block);
+                //if(block.Y == windowHeight)
+                //{
+                //    blocks.Remove(block);
+                //}
+
+            }
+            blocks.RemoveAll(block => player.X == block.X && player.Y == block.Y); // not sure how this works so look into it 
+
+            if (blockTimeElapsed >= blockSpawnTimer)
+            {
+                int speed = random.Next(1, 3);
+                int x = random.Next(0, windowWidth);
+                blocks.Add(new Block(x, 0, "X", speed));
+                blockTimeElapsed = 0;
+            }
+        }
+
+        private static void Draw()
+        {
+            Console.Clear();
+
+            player.Draw();
+
+            foreach (Block block in blocks)
+            {
+                block.Draw();
+            }
+            Console.SetCursorPosition(0, 21);
+            Console.WriteLine($"Time elapsed (ms): {Math.Round(gameTimeElapsed / 1000)}");
+            Console.WriteLine($"Player HP: {player.HP}");
+        }
+
+        
+    }
+}
